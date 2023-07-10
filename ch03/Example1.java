8f685c8f5ba074072c57cbb67e98ea5d63f67d8f
sangmin0816 <sangmin990816@gmail.com>
2023-07-10 14:26:39 +0900

    can't commit

--- ch03/Example1.java
+++ ch03/Example1.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Example1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        System.out.println("이름을 입력하세요");
+        String name = sc.nextLine();
+        System.out.println("나이를 입력하세요");
+        int age = sc.nextInt();
+        System.out.println("키를 입력하세요");
+        double height = sc.nextDouble();
+
+
+        sc.close();
     }
 }
