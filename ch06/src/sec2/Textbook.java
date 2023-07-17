49990389636c167fa04d7980c87a55ae7fb3bad3
sangmin0816 <sangmin990816@gmail.com>
2023-07-17 20:23:52 +0900

    all right java

--- ch06/src/sec2/Textbook.java
+++ ch06/src/sec2/Textbook.java
@@ -1,2 +1,36 @@
-package sec2;public class Textbook {
+package sec2;
+
+abstract class AbstractParent{
+    abstract void hello();
+}
+
+class Child1 extends AbstractParent{
+    @Override
+    void hello() {
+        System.out.println("Hello Child1");
+    }
+}
+
+class Child2 extends AbstractParent{
+    @Override
+    void hello() {
+        System.out.println("Hello Child2");
+    }
+}
+
+public class Textbook {
+    public static void main(String[] args) {
+        AbstractParent ap = new Child1();
+        ap.hello(); // Hello. Child1
+        ap = new Child2();
+        ap.hello(); // Hello. Child2
+        AbstractParent ap2 = new AbstractParent() {
+            @Override
+            void hello() {
+                System.out.println("Hello AbstractParent");
+            }
+        };
+        ap2.hello(); // Hello AbstractParent
+
+    }
 }
